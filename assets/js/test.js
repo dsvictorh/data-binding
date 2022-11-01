f7ea3c356dd878823d1d080b5ea3ca92104efdf1
Victor Herrera <dsvictorh@gmail.com>
2022-10-31 22:45:22 -0600

    Fix further issues with RadioNodeList items.

--- assets/js/test.js
+++ assets/js/test.js
@@ -2,23 +2,23 @@ window.onload = () => {
     new ViewModel();
 }
 
-class ViewModel{
-    constructor(){
+class ViewModel {
+    constructor() {
         this.count = new OneWayProp(0);
-        this.text = new TwoWayProp(null, 'string');
+        this.text = new TwoWayProp('Hola', 'string');
         this.select = new TwoWayProp(1);
         this.radio = new TwoWayProp(1);
-        this.items = new OneWayCollectionProp([{name: 'Victor'}, {name: 'Carlos'}, {name: 'Luis'}]);
+        this.items = new OneWayCollectionProp([{ name: 'Victor', age: 30 }, { name: 'Carlos', age: 31 }, { name: 'Luis', age: 32 }]);
 
         this.interval = setInterval(() => {
             this.count.value = this.count.value + 1;
-            if(this.count.value == 10){
+            if (this.count.value == 10) {
                 clearInterval(this.interval);
             }
         }, 1000);
 
         setTimeout(() => {
-            this.items.value = [{name: 'Victor'}, {name: 'Carlos'}, {name: 'Luis'}, {name: 'Diana'}];
+            this.items.value = [{ name: 'Victor', age: 30 }, { name: 'Carlos', age: 31 }, { name: 'Luis', age: 32 }, { name: 'Diana', age: 33 }];
             this.radio.value = 2;
         }, 1000);
 
@@ -39,14 +39,12 @@ class ViewModel{
     }
 }
 
-
-
-class OneWayProp{
-    #observers;    
+class OneWayProp {
+    #observers;
     #value;
     #type;
 
-    constructor(value, type, formatFunctions){
+    constructor(value, type, formatFunctions) {
         //Subscribed DOM elements
         this.#observers = [];
 
@@ -60,7 +58,7 @@ class OneWayProp{
         this._formatFunctions = formatFunctions;
     }
 
-    get value(){
+    get value() {
         return this.#value;
     }
 
@@ -69,46 +67,46 @@ class OneWayProp{
     }
 
     //Add single DOM element to observers and set its value immediately after subscription
-    subscribe(element){
+    subscribe(element) {
         this.#observers.push(element);
         this._setElementValue(element, this.#value);
     }
 
     //Add severals DOM elements to observers and set their value immediately after subscription
-    subscribeMany(elements){
-        for(let i = 0; i < elements.length; i++){
-            this.subscribe(elements[i]);
+    subscribeMany(elements) {
+        for (const element of elements) {
+            this.subscribe(element);
         }
     }
-    
-    _set(value, origin){
+
+    _set(value, origin) {
         //If property doesn't specify the type it is automatically grabbed
         //by the passed value of this function, hence the type won't be restricted
         let type = this.#type != null ? this.#type : typeof value;
 
-        switch(type){
+        switch (type) {
             case 'string':
-                if(value != null){
+                if (value != null) {
                     value = value.toString();
                 }
                 break;
             case 'number':
-                if(value != null){
+                if (value != null) {
                     value = parseFloat(value);
-                    if(isNaN(value)){
+                    if (isNaN(value)) {
                         throw new Error('Tried to set invalid number value');
                     }
                 }
                 break;
             case 'date':
                 value = value || null;
-                if(value != null){
+                if (value != null) {
                     value = new Date(value);
 
                     //If a wrong parameter is passed to instantiate a date 
                     //getTime() not having an epoch will fails isNaN checks
                     //therefore the Date is in an invalid state.
-                    if(isNaN(value.getTime())) {
+                    if (isNaN(value.getTime())) {
                         throw new Error('Tried to set invalid date value');
                     }
                 }
@@ -116,8 +114,8 @@ class OneWayProp{
             case 'boolean':
                 //Many DOM elements don't handle true or false but rather empty values
                 //so we need to make sure we account for these
-                if(typeof value !== 'boolean'){
-                    switch(value){
+                if (typeof value !== 'boolean') {
+                    switch (value) {
                         case 'true':
                             value = true;
                             break;
@@ -126,7 +124,7 @@ class OneWayProp{
                             break;
                         case '':
                         case null:
-                            value = null; 
+                            value = null;
                             break;
                         default:
                             throw new Error('Tried to set invalid boolean value');
@@ -141,29 +139,35 @@ class OneWayProp{
         //came originally from, or excluding none if the property was changed programatically. This is to avoid
         //chain reactions in the functionality.
         const observers = this.#observers.filter((element) => origin == null || element != origin);
-        for(let i = 0; i < observers.length; i++){
-            this._setElementValue(observers[i], value);
+        for (const observer of observers) {
+            this._setElementValue(observer, value);
         }
     }
 
-    _setElementValue(element, value){
+    _setElementValue(element, value) {
         //The value needs to be formatted with one of the format functions if said element
         ///specified using any of them in the collection. If not the element's value is set
         //equally to the property value
-        if(this._formatFunctions && element.matches('[data-format]')){
+        if (this._formatFunctions && element.matches('[data-format]')) {
             value = this._formatFunctions[element.getAttribute('data-format')](value);
         }
 
-        switch(element.tagName){
+        switch (element.tagName) {
             case 'INPUT':
-                if(element.matches('[type="checkbox"], [type="radio"]')){
-                    element.checked = element.value == value;
-                }else{
-                    element.value = value;
+                switch (element.getAttribute('type')) {
+                    case 'radio':
+                        element.checked = element.value == (value ?? '');
+                        break;
+                    case 'checkbox':
+                        element.checked = value;
+                        break;
+                    default:
+                        element.value = value;
+                        break;
                 }
                 break;
             case 'SELECT':
-                element.value = value != null ? value : '';
+                element.value = value ?? '';
                 break;
             case 'TEXTAREA':
                 element.value = value;
@@ -171,9 +175,11 @@ class OneWayProp{
             default:
                 //RadioNodeList is the type when using radio buttons through the DOM forms object
                 //and its value functions as a single property
-                if(element instanceof RadioNodeList){
-                    element.value = value;
-                }else{
+                if (element instanceof RadioNodeList) {
+                    for (const radio of element) {
+                        radio.checked = radio.value == (value ?? '');
+                    }
+                } else {
                     element.textContent = value;
                 }
                 break;
@@ -181,33 +187,35 @@ class OneWayProp{
     }
 }
 
-class TwoWayProp extends OneWayProp{
-    constructor(value, type, formatFunctions){
+class TwoWayProp extends OneWayProp {
+    constructor(value, type, formatFunctions) {
         super(value, type, formatFunctions);
     }
 
-    subscribe(element){
+    subscribe(element) {
         super.subscribe(element);
         this.#addTwoWay(element);
     }
 
-    subscribeMany(elements){
+    subscribeMany(elements) {
         super.subscribeMany(elements);
-        for(let i = 0; i < elements.length; i++){
-            this.#addTwoWay(elements[i]);
+        for (const element of elements) {
+            this.#addTwoWay(element);
         }
     }
 
     //Create event listeners for all observer DOM elements to change back the value of the property
     //when their value changes, hence, implementing two-way binding
-    #addTwoWay(element){
-        switch(element.tagName){
+    #addTwoWay(element) {
+        switch (element.tagName) {
             case 'INPUT':
-                if(element.matches('[type="checkbox"], [type="radio"]')){ 
+                //Radios on HTML don't handle the concept of null values, only empty strings.
+                //On empty values we need to force the null value upon the property
+                if (element.matches('[type="checkbox"], [type="radio"]')) {
                     element.addEventListener('change', (e) => {
-                        this._set(e.target.value, e.target);
+                        this._set(e.target.value != '' ? e.target.value : null, e.target);
                     });
-                }else{
+                } else {
                     element.addEventListener('input', (e) => {
                         this._set(e.target.value, e.target);
                     });
@@ -226,69 +234,82 @@ class TwoWayProp extends OneWayProp{
                 });
                 break;
             default:
-                element.addEventListener('change', (e) => {
-                    this._set(e.target.value, e.target);
-                });
+                //RadioNodeList is the type when using radio buttons through the DOM forms object
+                //and its value functions as a single property
+                if (element instanceof RadioNodeList) {
+                    for (const radio of element) {
+                        //Radios on HTML don't handle the concept of null values, only empty strings.
+                        //On empty values we need to force the null value upon the property
+                        radio.addEventListener('change', (e) => {
+                            this._set(e.target.value != '' ? e.target.value : null, element);
+                        });
+                    }
+                } else {
+                    element.addEventListener('change', (e) => {
+                        this._set(e.target.value, e.target);
+                    });
+                }
+
                 break;
         }
     }
 }
 
-class OneWayCollectionProp extends OneWayProp{
-    constructor(value, formatFunctions){
+class OneWayCollectionProp extends OneWayProp {
+    constructor(value, formatFunctions) {
         super(value, 'array', formatFunctions);
-        if(!Array.isArray(value)){
+        if (!Array.isArray(value)) {
             throw new Error('Parameter value is not of type array');
         }
     }
 
-    subscribe(element){
+    subscribe(element) {
         //Collection bindings work through HTML templates and are required.
-        if(!element.getAttribute('data-template')){
+        if (!element.getAttribute('data-template')) {
             throw new Error(`Subscriber element ${element.tagName} does not have required data-template attribute`);
         }
 
         super.subscribe(element);
     }
 
-    subscribeMany(elements){
-        for(let i = 0; i < elements.length; i++){
+    subscribeMany(elements) {
+        for (const element of elements) {
             //Collection bindings work through HTML templates and are required.
-            if(!elements[i].getAttribute('data-template')){
-                throw new Error(`Subscriber element ${elements[i].tagName} does not have required data-template attribute`);
+            if (!element.getAttribute('data-template')) {
+                throw new Error(`Subscriber element ${element.tagName} does not have required data-template attribute`);
             }
 
-            super.subscribe(elements[i]);
+            super.subscribe(element);
         }
     }
 
-    _setElementValue(element, collection){
+    _setElementValue(element, collection) {
         //Grab the specified template for the observer DOM element
         let template = document.querySelector('#' + element.getAttribute('data-template'));
 
         //Reset the inner HTML structure since it needs to refresh according to the collection
         element.innerHTML = '';
-        for(let item of collection){
+        for (let item of collection) {
             //Make a copy of the template HTML structure for one item in the collection
             const node = template.content.cloneNode(true);
-            
+
             //Grab all value attribute elements from the template that need to be filled with
             //the data provided by the current item in the collection
-            for(const bindingChild of node.querySelectorAll('[data-value]')){
-                const prop = bindingChild.getAttribute('data-value'); 
+            for (const bindingChild of node.querySelectorAll('[data-value]')) {
+                const prop = bindingChild.getAttribute('data-value');
                 let value;
 
-                if(prop){
+                if (prop) {
                     //Properties can come from complex objects with several levels
                     //so the get property function recursively gets to the end value
                     //property separated by "." just like a JS object
                     value = this.#getProperty(item, prop.split('.'));
-                }else{
+                } else {
                     value = item;
                 }
 
                 //Format functions can also be bound in a specific data item on the template
-                if(this._formatFunctions && bindingChild.matches('[data-format]')){
+                if (this._formatFunctions && bindingChild.matches('[data-format]')) {
                     value = this._formatFunctions[bindingChild.getAttribute('data-format')](value);
                 }
 
@@ -296,32 +317,32 @@ class OneWayCollectionProp extends OneWayProp{
             }
 
             //Properties or values in the collection can be set into an attribute instead of the text node
-            for(const bindingChild of node.querySelectorAll('[data-attr]')){
+            for (const bindingChild of node.querySelectorAll('[data-attr]')) {
                 const attrs = bindingChild.getAttribute('data-attr');
                 const validRegex = /^[ ]*[^:\s]+[ ]*(:[ ]*([A-z]|_|\$)+([A-z]|_|\$|[0-9]|\.)*[ ]*)*(\,[ ]*[^:\s]+[ ]*(:[ ]*([A-z]|_|\$)+([A-z]|_|\$|[0-9]|\.)*[ ]*)*)*[ ]*$/gs;
 
                 //Validate through regex that data-attr follows the correct pattern
-                if(!attrs.match(validRegex)){
+                if (!attrs.match(validRegex)) {
                     throw new Error(`Property data-attr for element ${element.tagName} has invalid format`)
                 }
 
                 //Separate all attribute mappings
-                for(const attr of attrs.split(',')){
+                for (const attr of attrs.split(',')) {
                     //Create key-value pairs
                     const pair = attr.split(':');
                     const attribute = pair[0].trim();
                     let value;
 
-                    if(pair.length == 2){
+                    if (pair.length == 2) {
                         //Properties can come from complex objects with several levels
                         //so the get property function recursively gets to the end value
                         //property separated by "." just like a JS object
                         value = this.#getProperty(item, pair[1].trim().split('.'));
-                    }else{
+                    } else {
                         value = item;
                     }
 
-                    bindingChild.setAttribute(attribute, value);
+                    bindingChild.setAttribute(attribute, value ?? '');
                 }
             }
 
@@ -331,8 +352,8 @@ class OneWayCollectionProp extends OneWayProp{
         }
     }
 
-    #getProperty(value, propStructure){ 
-        for(let prop of propStructure){
+    #getProperty(value, propStructure) {
+        for (const prop of propStructure) {
             value = value[prop];
         }
 
